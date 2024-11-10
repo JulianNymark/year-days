4ac057c33d1f73733823579afa5feef2342621a9
Julian Nymark <j@juliannymark.com>
2024-11-10 18:36:58 +0100

    :sparkles: check button

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,18 @@ const Day = () => {
   return <input type="checkbox" />;
 };
 
-const Year = ({ year, justifyEnd }: { year: string; justifyEnd?: boolean }) => {
+const Year = ({
+  year,
+  justifyEnd,
+  leapYear,
+}: {
+  year: string;
+  justifyEnd?: boolean;
+  leapYear?: boolean;
+}) => {
   const months = [
     { days: 31, name: "jan" },
-    { days: 28, name: "feb" },
+    { days: 28 + (!!leapYear ? 1 : 0), name: "feb" },
     { days: 31, name: "mar" },
     { days: 30, name: "apr" },
     { days: 31, name: "may" },
@@ -17,10 +25,9 @@ const Year = ({ year, justifyEnd }: { year: string; justifyEnd?: boolean }) => {
     { days: 30, name: "nov" },
     { days: 31, name: "dec" },
   ];
-  console.log(months.reduce((acc, curr) => acc + curr, 0));
 
   return (
-    <div className={`flex w-full ${justifyEnd ? "justify-end" : ""}`}>
+    <div className={`flex m-4 w-full ${justifyEnd ? "justify-end" : ""}`}>
       <div className="flex w-fit justify-center">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl">{year}</h1>
@@ -48,9 +55,30 @@ const Year = ({ year, justifyEnd }: { year: string; justifyEnd?: boolean }) => {
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2">
-      <Year year={"2024"} justifyEnd />
-      <Year year={"2025"} />
+    <div>
+      <div className="grid grid-cols-2">
+        <Year year={"2024"} justifyEnd />
+        <Year year={"2025"} />
+      </div>
+      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <style>{`
+.my_button {
+  border-radius: 8px;
+  border: 1px solid oklch(90% 0 100);
+  background: oklch(90% 0 100);
+  color: black;
+  padding-inline: 20px;
+  padding-block: 10px;
+}
+
+.my_button:hover {
+  background: oklch(100% 0 100);
+  box-shadow: 0 0 20px 2px white;
+}
+
+`}</style>
+        <button className="my_button">check today</button>
+      </div>
     </div>
   );
 }
